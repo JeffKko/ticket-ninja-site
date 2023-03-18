01fa8fa05ce95b9181ffe6c07a90c4cf6a3f6da7
Jeff <b3589481400@gmail.com>
2023-03-19 02:01:19 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import DescriptionIcon from '@mui/icons-material/Description';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import Link from '../src/Link';
+import ProTip from '../src/ProTip';
+import Copyright from '../src/Copyright';
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI - Next.js example in TypeScript
+        </Typography>
+        <h1>Material UI - Next.js example in TypeScript</h1>
+        <h4>Material UI - Next.js example in TypeScript</h4>
+        <Typography variant="subtitle1" align="center" gutterBottom>
+          TICKET.Ninja 是一個購票輔助程式 <br />
+          專門為拓元售票系統
+          (<Link href="https://tixcraft.com" underline="hover">{'https://tixcraft.com'}</Link>) 打造 <br />
+          幫助您擊敗搶票機器人和搶票外掛 <br />
+          遠離黃牛, 輕鬆搶票
+        </Typography>
+
+        <Box
+          sx={{
+            my: 4,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Button variant="text" component={Link} href="/guide" color="secondary" startIcon={<DescriptionIcon />}>
+            使用說明
+          </Button>
+          ．
+          <Button variant="text" component={Link} href="/question" color="secondary" startIcon={<QuestionAnswerIcon />}>
+            常見問題
+          </Button>
+        </Box>
+
+        {/* <Link href="/about" color="secondary">
+          Go to the about page
+        </Link> */}
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#282c34',
+    },
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+    subtitle1: {
+      fontWeight: 600,
+    }
+  },
+});
+
+export default theme;
